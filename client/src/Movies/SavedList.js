6435107671a889e9c5096cec490845adb7fde3c9
Jor_Poon <jormpoon@gmail.com>
2019-02-26 19:31:48 -0500

    anchor tag text decoration

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -17,7 +17,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link  to={`/movies/${movie.id}`} key={movie.id} className="saved-movie">{movie.title}</Link>
+          <NavLink exact to={`/movies/${movie.id}`} key={movie.id} className="saved-movie">{movie.title}</NavLink>
         ))}
         <button onClick={this.toHome} className="home-button">Home</button>
       </div>
